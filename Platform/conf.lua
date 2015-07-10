1154cb852e1b17c62ecaed9f31ca6ed10734cb37
Rob Probin <rob.probin@gmail.com>
2015-07-10 17:32:53 +0100

    Another fix for Love2D 0.9.

--- Platform/conf.lua
+++ Platform/conf.lua
@@ -1,5 +1,7 @@
 -- see https://love2d.org/wiki/Config_Files
 function love.conf(t)
+    t.window = t.window or t.screen
+    
     t.title = "Platform"        -- The title of the window the game is in (string)
     t.author = "Rob Probin"     -- The author of the game (string)
 	-- t.url = nil                 -- The website of the game (string)
@@ -7,9 +9,11 @@ function love.conf(t)
     --t.version = "0.8.0"         -- The LÖVE version this game was made for (string)
     --t.console = false           -- Attach a console (boolean, Windows only)
     --t.release = false           -- Enable release mode (boolean)
-    t.screen.width = 1024       -- The window width (number)
-    t.screen.height = 768       -- The window height (number)
+    t.window.width = 1024       -- The window width (number)
+    t.window.height = 768       -- The window height (number)
 	-- t.screen.fullscreen = false -- Enable fullscreen (boolean)
+    
+    t.screen = t.screen or t.window
 end
 
 
